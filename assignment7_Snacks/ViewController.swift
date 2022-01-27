45c407665629e550f224e66299d3039dbd87a4a8
sasalin777 <candysasame@gmail.com>
2022-01-26 22:55:43 -0800

    updated

--- assignment7_Snacks/ViewController.swift
+++ assignment7_Snacks/ViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
-   
+    @IBOutlet var plussign: UIButton!
+    
     @IBOutlet var heightChange: NSLayoutConstraint!
     @IBOutlet var navBarView: UIView!
     override func viewDidLoad() {
@@ -19,8 +21,32 @@ class ViewController: UIViewController {
 
     @IBAction func plusPressed(_ sender: UIButton) {
         print("plus icon pressed")
-        heightChange.constant = 200.0
-       
+        //heightChange.constant = 200.0
+        heightChanged()
+        plussign.tintColor = .systemBlue
+        plussign.transform = .init(rotationAngle: 90.0)
+        
+        
+        }
+    
+  func heightChanged() {
+
+        navBarView = UIView(frame: .init(x: 0, y: 44, width: 414 , height: 88))
+
+       view.addSubview(navBarView)
+    
+
+      UIView.animate(withDuration: 3.0, delay: 0.2, usingSpringWithDamping: 0.3, initialSpringVelocity: 3) { [self] in
+
+          self.navBarView.bounds = CGRect(x: 0, y: 44, width: 414, height: 200)
+          self.navBarView.backgroundColor = UIColor(red: 0.8667, green: 0.8667, blue: 0.8667, alpha: 1.0)
+          //self.navBarView.backgroundColor = .blue
+
+
+        }
     }
+   
+   
 }
+
     
